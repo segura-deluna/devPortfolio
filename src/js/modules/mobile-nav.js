357f471babeda44fa81b1ab840d35c8502d281f9
Segura <segura-deluna@mail.ru>
2024-09-05 13:53:58 +0300

    mobile finish

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,23 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  // Mobile nav button
+  const navBtn = document.querySelector('.mobile-nav-btn')
+  const nav = document.querySelector('.mobile-nav')
+  const menuIcon = document.querySelector('.nav-icon')
+  const navLinks = document.querySelectorAll('.mobile-nav a')
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  navBtn.onclick = function() {
+    nav.classList.toggle('mobile-nav--open')
+    menuIcon.classList.toggle('nav-icon--active')
+    document.body.classList.toggle('no-scroll')
+  }
+
+  navLinks.forEach((el) => {
+    el.addEventListener('click', () => {
+      nav.classList.remove('mobile-nav--open')
+      document.body.classList.remove('no-scroll')
+      menuIcon?.classList.remove('nav-icon--active')
+    })
+  })
 }
 
-export default mobileNav;
+export default mobileNav
